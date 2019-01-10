2151901254c7a9239f8f8f7802286d997d0d0862
Four5Fire <942558046@qq.com>
2019-01-10 16:28:54 +0800

    tijiao

--- src/main/java/cn/playcall/videomark/interceptor/VideoInterceptor.java
+++ src/main/java/cn/playcall/videomark/interceptor/VideoInterceptor.java
@@ -1,5 +1,7 @@
 package cn.playcall.videomark.interceptor;
 
+import cn.playcall.videomark.entity.UserInfo;
+import cn.playcall.videomark.server.HttpClient;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +16,12 @@ public class VideoInterceptor extends HandlerInterceptorAdapter {
         if (session == null){
             System.out.println("Illegal user login");
             return false;
+        }else {
+            UserInfo userInfo = (UserInfo) session.getAttribute("userInfo");
+            if (HttpClient.getTaskFiles(userInfo)){
+                session.setAttribute("userInfo",userInfo);
+                System.out.println("信息更新成功");
+            }
         }
         return true;
     }
